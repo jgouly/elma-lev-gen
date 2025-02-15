d6ba20467b97ecc5949757555c640bed03d5374f
Joey Gouly <joey.gouly@gmail.com>
2025-02-15 18:38:26 +0000

    WIP: basic spikes

--- src/flat.rs
+++ src/flat.rs
@@ -6,6 +6,7 @@ use elma::Position;
 pub struct FlatTrackConfig {
     width: f64,
     height: f64,
+    num_segments: f64,
 }
 
 impl Default for FlatTrackConfig {
@@ -13,8 +14,20 @@ impl Default for FlatTrackConfig {
         Self {
             width: 50.0,
             height: 7.0,
+            num_segments: 40.0,
         }
     }
+}
+
+fn gen_spike(
+    x_offset: f64,
+    width: f64,
+) -> [Position<f64>; 3] {
+    [
+        Position::new(x_offset + (width / 2.0), 0.0),
+        Position::new(x_offset, 0.5),
+        Position::new(x_offset - (width / 2.0), 0.0),
+    ]
 }
 
 fn gen_polygons(config: &FlatTrackConfig) -> Vec<Polygon> {
@@ -28,6 +41,29 @@ fn gen_polygons(config: &FlatTrackConfig) -> Vec<Polygon> {
     // Right wall
     vertices.push(Position::new(width, height));
     vertices.push(Position::new(width, 0.));
+
+    let start_x = 3.0;
+    let end_x = width - 2.0;
+
+    let n_segments = config.num_segments;
+    let full_segments_width = end_x - start_x;
+    let segment_width = full_segments_width / n_segments;
+
+    let mut centre_of_spikes = vec![];
+    let mut centre = end_x - (segment_width / 2.0);
+    while centre > start_x {
+        centre_of_spikes.push(centre);
+        centre -= segment_width;
+    }
+    centre_of_spikes.pop();
+
+    vertices.extend(
+        centre_of_spikes
+            .iter()
+            .flat_map(|&c| gen_spike(c, segment_width)),
+    );
+    vertices.push(Position::new(4.0, 0.0));
+    vertices.dedup_by(|a, b| (a.x - b.x).abs() < 0.005 && (a.y - b.y).abs() < 0.005);
 
     vec![Polygon {
         grass: false,
