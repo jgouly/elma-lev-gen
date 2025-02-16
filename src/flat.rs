95c67e1359848f4cad791a233f22dc9a11cdb650
Joey Gouly <joey.gouly@gmail.com>
2025-02-16 13:01:07 +0000

    WIP: abstract out config

--- src/flat.rs
+++ src/flat.rs
@@ -4,11 +4,30 @@ use elma::Position;
 use rand::Rng;
 
 #[derive(Clone, Debug)]
+struct SegmentConfig {
+    num_segments: f64,
+    spike_height_range: std::ops::Range<f64>,
+}
+
+impl Default for SegmentConfig {
+    fn default() -> Self {
+        Self {
+            num_segments: 40.0,
+            spike_height_range: -0.4..1.0,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+enum TypeConfig {
+    SegmentConfig(SegmentConfig),
+}
+
+#[derive(Clone, Debug)]
 pub struct FlatTrackConfig {
     width: f64,
     height: f64,
-    num_segments: f64,
-    spike_height_range: std::ops::Range<f64>,
+    type_config: TypeConfig,
 }
 
 impl Default for FlatTrackConfig {
@@ -16,28 +35,31 @@ impl Default for FlatTrackConfig {
         Self {
             width: 50.0,
             height: 7.0,
-            num_segments: 40.0,
-            spike_height_range: -0.4..1.0,
+            type_config: TypeConfig::SegmentConfig(SegmentConfig::default()),
         }
     }
 }
 
 fn gen_spike(
     config: &FlatTrackConfig,
+    sc: &SegmentConfig,
     x_offset: f64,
     width: f64,
     rng: &mut impl Rng,
 ) -> [Position<f64>; 3] {
     [
         Position::new(x_offset + (width / 2.0), 0.0),
-        Position::new(x_offset, rng.gen_range(config.spike_height_range.clone())),
+        Position::new(x_offset, rng.gen_range(sc.spike_height_range.clone())),
         Position::new(x_offset - (width / 2.0), 0.0),
     ]
 }
 
-fn gen_polygons(config: &FlatTrackConfig, rng: &mut impl Rng) -> Vec<Polygon> {
+fn gen_segments(config: &FlatTrackConfig, rng: &mut impl Rng) -> Vec<Polygon> {
     let width = config.width;
     let height = config.height;
+    let TypeConfig::SegmentConfig(sc) = &config.type_config else {
+        unreachable!()
+    };
     let mut vertices = vec![];
 
     // Left wall
@@ -50,7 +72,7 @@ fn gen_polygons(config: &FlatTrackConfig, rng: &mut impl Rng) -> Vec<Polygon> {
     let start_x = 3.0;
     let end_x = width - 2.0;
 
-    let n_segments = config.num_segments;
+    let n_segments = sc.num_segments;
     let full_segments_width = end_x - start_x;
     let segment_width = full_segments_width / n_segments;
 
@@ -65,7 +87,7 @@ fn gen_polygons(config: &FlatTrackConfig, rng: &mut impl Rng) -> Vec<Polygon> {
     vertices.extend(
         centre_of_spikes
             .iter()
-            .flat_map(|&c| gen_spike(config, c, segment_width, rng)),
+            .flat_map(|&c| gen_spike(config, sc, c, segment_width, rng)),
     );
     vertices.push(Position::new(4.0, 0.0));
     vertices.dedup_by(|a, b| (a.x - b.x).abs() < 0.005 && (a.y - b.y).abs() < 0.005);
@@ -79,7 +101,9 @@ fn gen_polygons(config: &FlatTrackConfig, rng: &mut impl Rng) -> Vec<Polygon> {
 pub fn gen(rng: &mut impl Rng) -> Level {
     let config = FlatTrackConfig::default();
     let mut level = Level::new();
-    level.polygons = gen_polygons(&config, rng);
+    level.polygons = match config.type_config {
+        TypeConfig::SegmentConfig(_) => gen_segments(&config, rng),
+    };
     level.objects = vec![
         Object {
             position: Position::new(1.0, 0.0 + OBJECT_RADIUS),
